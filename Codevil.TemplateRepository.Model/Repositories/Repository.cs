0b9e5dfd14712fd64464701daf9f679b98801b49
macskeptic <macskeptic@gmail.com>
2009-06-20 01:41:49 -0300

    added alias for ambiguous reference

--- Codevil.TemplateRepository.Model/Repositories/Repository.cs
+++ Codevil.TemplateRepository.Model/Repositories/Repository.cs
@@ -6,10 +6,11 @@ using Codevil.TemplateRepository.Model.Entities;
 using Codevil.TemplateRepository.Entities;
 using Codevil.TemplateRepository.Data.Factories;
 using Codevil.TemplateRepository.Model.Factories;
+using Template = Codevil.TemplateRepository.Repositories;
 
 namespace Codevil.TemplateRepository.Model.Repositories
 {
-    public abstract class Repository<TRow, TEntity> : Codevil.TemplateRepository.Repositories.Repository<TRow, TEntity>
+    public abstract class Repository<TRow, TEntity> : Template.Repository<TRow, TEntity>
         where TRow : class
         where TEntity : DataEntity
     {
